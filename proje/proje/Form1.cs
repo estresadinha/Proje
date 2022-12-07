ca11092e91ba4f8e60a63b5984bbdd8e6278396b
estresadinha <120022356+estresadinha@users.noreply.github.com>
2022-12-07 10:46:54 -0300

    Add files via upload

---
+++ proje/proje/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace proje
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            mensagem1.Visible = false;
+        }
+
+
+        private void botao1_Click(object sender, EventArgs e)
+        {
+            mensagem1.Visible = true;
+            mensagem1.Text = "Agora, clique em mim para alterar a cor do botão";
+
+        }
+
+        private void mensagemClicada(object sender, EventArgs e)
+        {
+            botao1.BackColor = Color.Yellow;
+            mensagem1.Visible=false;
+
+        }
+    }
+}
